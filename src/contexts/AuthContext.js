beb27da678d9ff4bc11e2920c44ae1b9959bf1ce
Avinash Kumar <avinash9k5r@gmail.com>
2021-06-01 19:42:20 +0530

    add other files

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,9 +14,23 @@ export function AuthProvider({ children }) {
   }
 
   function login(email, password) {
-    return auth.signInWithCredential(email, password);
+    return auth.signInWithEmailAndPassword(email, password);
   }
 
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -33,6 +47,10 @@ export function AuthProvider({ children }) {
     currentUser,
     login,
     signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
   };
   return (
     <AuthContext.Provider value={value}>
